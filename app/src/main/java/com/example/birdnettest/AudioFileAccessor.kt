20b241ec603adc36b70dda873e50880ac48469af
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-02-06 03:05:27 -0800

    Created a class to access audio files from MediaStore API

---
+++ app/src/main/java/com/example/birdnettest/AudioFileAccessor.kt
@@ -0,0 +1,66 @@
+package com.example.birdnettest
+
+import android.content.ContentResolver
+import android.content.ContentUris
+import android.net.Uri
+import android.provider.MediaStore
+
+class AudioFileAccessor {
+
+    // Container for information about each audio file
+    data class AudioFile(
+        val uri: Uri, // check if this is needed
+        //val id: Long,
+        val title: String,
+        val data: String
+        //val mimeType: String
+    )
+
+    fun getAudioFiles(contentResolver: ContentResolver): List<AudioFile> {
+
+        val audioFiles = mutableListOf<AudioFile>()
+
+        val collection = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DATA,
+        )
+
+        // Select audio files with the wav extension (continue to add on valid types to array)
+        val selection = "${MediaStore.Audio.Media.MIME_TYPE} = ?"
+        val selectionArgs = arrayOf("audio/wav")
+
+        val query = contentResolver.query(
+            collection,
+            projection,
+            selection,
+            selectionArgs,
+            null
+        )
+
+        query?.use {
+            val idColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val dataColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+
+            while (it.moveToNext()) {
+                // Get values of column for a given audio file
+                val id = it.getLong(idColumn)
+                val title = it.getString(titleColumn)
+                val data = it.getString(dataColumn)
+
+                val audioUri: Uri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id
+                )
+
+                audioFiles.add(AudioFile(audioUri, title, data))
+            }
+
+            it.close()
+        }
+
+        return audioFiles
+    }
+}
