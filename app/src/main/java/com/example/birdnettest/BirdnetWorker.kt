349a78d865bd8c746a45906fdd9b0cf42b90d462
OutisNoMore <OutisNoMore@protonmail.com>
2023-08-09 15:24:15 -0700

    log start of worker and end

--- app/src/main/java/com/example/birdnettest/BirdnetWorker.kt
+++ app/src/main/java/com/example/birdnettest/BirdnetWorker.kt
@@ -13,6 +13,33 @@ class BirdnetWorker (appContext: Context, workerParams: WorkerParameters): Worke
     private val ctx = appContext
 
     /*
+     * Write stats to log
+     */
+    fun writeToLog(start: java.util.Date) {
+        // Write to log
+        val totalFiles =
+            ctx.filesDir.list { _, name -> name.endsWith("-result.csv") }?.size
+                ?: 0
+        // Calculate how many new files were created
+        val prefs = ctx.getSharedPreferences("files_processed", Context.MODE_PRIVATE)
+        var newFiles = prefs.getInt("processed", 0)
+        newFiles = totalFiles - newFiles
+        with(prefs.edit()) {
+            putInt("processed", totalFiles)
+            apply() // asynchronous write to external memory
+        }
+        // Write to log
+        FileWriter("${ctx.filesDir}/AudioBird-Log.txt", true).use { out ->
+            out.write("\n------------------------------------------------------------------------\n")
+            out.write("BirdNET worker Started Successfully: $start\n")
+            out.write("BirdNET worker Completed Successfully: ${Calendar.getInstance().time}\n")
+            out.write("Total Files Found: $totalFiles\n")
+            out.write("New Files Processed: $newFiles\n")
+            out.write("--------------------------------------------------------------------------\n")
+        }
+    }
+
+    /*
      * Function that is run by task scheduler
      */
     override fun doWork(): Result {
@@ -20,14 +47,16 @@ class BirdnetWorker (appContext: Context, workerParams: WorkerParameters): Worke
         MediaScannerConnection.scanFile(
             ctx, arrayOf(Environment.getExternalStorageDirectory().path), null
         ) { path, uri_ ->
+            val start = Calendar.getInstance().time
             try {
                 util.runBirdNet()
                 // Write result to log
-                util.writeToLog(true)
+                writeToLog(start)
             } catch (e: Exception) {
                 e.printStackTrace()
                 FileWriter("${applicationContext.filesDir}/AudioBird-Log.txt", true).use { out ->
                     out.write("\n------------------------------------------------------------------------\n")
+                    out.write("BirdNET worker Started Successfully: $start\n")
                     out.write("BirdNET Worker Stopped: ${Calendar.getInstance().time}\n")
                     out.write("Failed with error: ${e.message}")
                     out.write("--------------------------------------------------------------------------\n")
