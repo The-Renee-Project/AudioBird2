3040f209b03eaa8755cc026ebb6d5ae3483da907
OutisNoMore <OutisNoMore@protonmail.com>
2023-08-15 11:23:43 -0700

    fix processing for worker

--- app/src/main/java/com/example/birdnettest/Util.kt
+++ app/src/main/java/com/example/birdnettest/Util.kt
@@ -30,7 +30,7 @@ class Util (appContext: Context) {
 
         for (file in audioFiles) {
             // Only process files if they haven't been processed before, or have been updated
-            if (!File(ctx.filesDir.toString(), "${file.title}-result.csv").exists()) {
+            if (!File(ctx.filesDir.toString(), "${file.title}-result.csv").exists() && !file.title.endsWith("-output")) {
                 // Classify birds from audio recording
                 val data = myBird.runTest(file.data)
                 // Only process data if it exists
